78e522a8fb418613fcfa8da2c5674ee62447df23
Michal Hozza <mhozza@gmail.com>
2016-06-10 15:09:35 +0200

    split to two components, added controls

--- tips/js_src/tips.jsx
+++ tips/js_src/tips.jsx
@@ -1,4 +1,15 @@
 class TipOfDay extends React.Component {
+  render() {
+    return <div className="tip_of_day">
+      <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <h1 className="tip_of_day_title">{this.props.title}</h1>
+      <div className="tip_of_day_text">{this.props.text}</div>
+      <div className="tip_of_day_controls"><a href="#" className="btn btn-link" onClick={this.props.onNext}>Next tip &raquo; </a></div>
+    </div>
+  }
+}
+
+class TipOfDayApp extends React.Component {
   constructor(props) {
     super(props);
     // Operations usually carried out in componentWillMount go here
@@ -9,19 +20,27 @@ class TipOfDay extends React.Component {
   }
 
   getCurrentTip() {
-    $.getJSON(TIPS_URL_ROOT + 'get_current_tip/', (data) => this.setState({data: data}))
+    $.getJSON(`${TIPS_URL_ROOT}get_current_tip/`, data => this.setState({data: data}))
+  }
+
+  markRead(id, callback) {
+    // $.getJSON(`${TIPS_URL_ROOT}mark_tip_as_read/${id}`, data => this.setState({data: data}))
+    callback()
+  }
+
+  handleNext(event) {
+    this.markRead(this.state.data.id, () =>
+      this.getCurrentTip()
+    );
   }
 
   render() {
     if (this.state.data) {
-      return <div>
-        <h1>{this.state.data.title}</h1>
-        <div>{this.state.data.text}</div>
-        </div>
+      return <TipOfDay title={this.state.data.title} text={this.state.data.text} onNext={this.handleNext.bind(this)}/>
     } else {
-      return <span/>
+      return <span>Loading...</span>
     }
   }
 }
 
-ReactDOM.render(<TipOfDay/>, document.getElementById('tip_of_day_container'))
+ReactDOM.render(<TipOfDayApp/>, document.getElementById('tip_of_day_container'))
--- tips/static/tips/js/tips.js
+++ tips/static/tips/js/tips.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -11,54 +11,110 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var TipOfDay = function (_React$Component) {
   _inherits(TipOfDay, _React$Component);
 
-  function TipOfDay(props) {
+  function TipOfDay() {
     _classCallCheck(this, TipOfDay);
 
-    // Operations usually carried out in componentWillMount go here
-
-    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(TipOfDay).call(this, props));
-
-    _this.state = {
-      data: null
-    };
-    _this.getCurrentTip();
-    return _this;
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(TipOfDay).apply(this, arguments));
   }
 
   _createClass(TipOfDay, [{
-    key: 'getCurrentTip',
-    value: function getCurrentTip() {
-      var _this2 = this;
-
-      $.getJSON(TIPS_URL_ROOT + 'get_current_tip/', function (data) {
-        return _this2.setState({ data: data });
-      });
-    }
-  }, {
-    key: 'render',
+    key: "render",
     value: function render() {
-      if (this.state.data) {
-        return React.createElement(
-          'div',
-          null,
+      return React.createElement(
+        "div",
+        { className: "tip_of_day" },
+        React.createElement(
+          "button",
+          { type: "button", "class": "close", "aria-label": "Close" },
           React.createElement(
-            'h1',
-            null,
-            this.state.data.title
-          ),
-          React.createElement(
-            'div',
-            null,
-            this.state.data.text
+            "span",
+            { "aria-hidden": "true" },
+            "×"
           )
-        );
-      } else {
-        return React.createElement('span', null);
-      }
+        ),
+        React.createElement(
+          "h1",
+          { className: "tip_of_day_title" },
+          this.props.title
+        ),
+        React.createElement(
+          "div",
+          { className: "tip_of_day_text" },
+          this.props.text
+        ),
+        React.createElement(
+          "div",
+          { className: "tip_of_day_controls" },
+          React.createElement(
+            "a",
+            { href: "#", className: "btn btn-link", onClick: this.props.onNext },
+            "Next tip » "
+          )
+        )
+      );
     }
   }]);
 
   return TipOfDay;
 }(React.Component);
 
-ReactDOM.render(React.createElement(TipOfDay, null), document.getElementById('tip_of_day_container'));
+var TipOfDayApp = function (_React$Component2) {
+  _inherits(TipOfDayApp, _React$Component2);
+
+  function TipOfDayApp(props) {
+    _classCallCheck(this, TipOfDayApp);
+
+    // Operations usually carried out in componentWillMount go here
+
+    var _this2 = _possibleConstructorReturn(this, Object.getPrototypeOf(TipOfDayApp).call(this, props));
+
+    _this2.state = {
+      data: null
+    };
+    _this2.getCurrentTip();
+    return _this2;
+  }
+
+  _createClass(TipOfDayApp, [{
+    key: "getCurrentTip",
+    value: function getCurrentTip() {
+      var _this3 = this;
+
+      $.getJSON(TIPS_URL_ROOT + "get_current_tip/", function (data) {
+        return _this3.setState({ data: data });
+      });
+    }
+  }, {
+    key: "markRead",
+    value: function markRead(id, callback) {
+      // $.getJSON(`${TIPS_URL_ROOT}mark_tip_as_read/${id}`, data => this.setState({data: data}))
+      callback();
+    }
+  }, {
+    key: "handleNext",
+    value: function handleNext(event) {
+      var _this4 = this;
+
+      this.markRead(this.state.data.id, function () {
+        return _this4.getCurrentTip();
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.state.data) {
+        return React.createElement(TipOfDay, { title: this.state.data.title, text: this.state.data.text, onNext: this.handleNext.bind(this) });
+      } else {
+        return React.createElement(
+          "span",
+          null,
+          "Loading..."
+        );
+      }
+    }
+  }]);
+
+  return TipOfDayApp;
+}(React.Component);
+
+ReactDOM.render(React.createElement(TipOfDayApp, null), document.getElementById('tip_of_day_container'));
