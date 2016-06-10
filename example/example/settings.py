7b81eda9c1b0575e3ec6f2d185bb3f36c617f0dc
Michal Hozza <mhozza@gmail.com>
2016-06-10 18:09:35 +0200

    inclusion template

--- example/example/settings.py
+++ example/example/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'tips',
+    'sekizai',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'sekizai.context_processors.sekizai',
             ],
         },
     },
