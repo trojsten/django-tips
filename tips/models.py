a4677d3153fb5c02f1e5b750dc41b647250b5574
Michal Hozza <mhozza@gmail.com>
2016-06-10 18:08:46 +0200

    added model meta and markdown rendering

--- tips/models.py
+++ tips/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.db import models
 from django.conf import settings
+from django.db import models
+from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
+from markdown import markdown
 
 
 class TipOfDay(models.Model):
@@ -14,3 +16,15 @@ class TipOfDay(models.Model):
     )
     active = models.BooleanField(default=True)
     seen_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='seen_tips', blank=True)
+
+    class Meta:
+        verbose_name = 'tip'
+        verbose_name_plural = 'tips'
+
+    def __str__(self):
+        return self.title
+
+    def rendered_text(self):
+        return mark_safe(markdown(self.text, **getattr(
+            settings, 'TIPS_MARKDOWN_KWARGS', {}
+        )))
